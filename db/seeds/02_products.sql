35cf4ef8302534c75a50aeb435e95d0c20422573
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2020-11-28 17:52:18 +0000

    added a few more seeds

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -12,3 +12,18 @@ INSERT INTO products (name, picture, description, price, category) VALUES ('Cowb
 INSERT INTO products (name, picture, description, price, category) VALUES ('Burger Tower', 'undefinedURL', '6 quarter-pound beef patties, bacon, 3 slices of Monterey Jack & cheddar because screw that diet!', 32.95, "Burgers" )
 INSERT INTO products (name, picture, description, price, category) VALUES ('Porky', 'undefinedURL', 'Pulled pork, bacon, ham, Monterey Jack, smoky BBQ sauce. Try not to think of that adorable looney tunes Character while you eat this you psycho!', 20.95, "Burgers" )
 INSERT INTO products (name, picture, description, price, category) VALUES ('Southern comfort', 'undefinedURL', 'Crispy fried chicken, candied bacon, Monterey Jack cheese, maple mayonnaise. Served between two waffles. Your judgment means nothing to us.', 19.95, "Burgers" )
+
+INSERT INTO products (name, picture, description, price, category) VALUES ('Mac and Cheese', 'undefinedUrl', 'The classic you know and love. Ask for extra cheese, we wont judge', 9.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category) VALUES ('Poutine', 'undefinedUrl', 'fries, cheese curds and gravy. Can only be ordered in French', 9.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category) VALUES ('Smoked meat poutine', 'undefinedUrl', 'Pulled smoked meat on a classic poutine. Nuff said', 13.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category) VALUES ('Cheesy Dog', 'undefinedUrl', 'Mac N Cheese topped with baked mozzarella. You know you want two of these', 11.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category) VALUES ('Chicken Sub', 'undefinedUrl', 'Grilled chicken, mushrooms, green peppers, caramelized onions topped with aged Mozzarella (Legal age).', 15.95, 'Classics');
+
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Heineken', 'undefinedURL', 'Refreshing pale beer. Served in a coffee cup to anyone ordering before noon',  6.95, 'Beverages');
+
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Pabs Blue Ribbon', 'undefinedURL', 'American Lager. Buy two and get a free beanie',  6.95, 'Beverages');
+
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Heineken', 'undefinedURL', 'Refreshing pale beer. Served in a coffee cup to anyone ordering before noon',  6.95, 'Beverages');
