eef09f064516240fd1c2470dd746671cb8a3898f
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2020-11-29 18:26:18 +0000

    register route semi-functional

--- server.js
+++ server.js
@@ -71,15 +71,15 @@ app.get('/register', (req, res) => {
 ////// WILL NEED to add template vars to use the newly aquired user information and add it to NAV to show
 app.post('/register', (req, res) => {
   console.log(req.body.name, req.body.phone, req.body.email, req.body.password)
-  if (getUserWithEmail(req.body.email)) {
-    res.send("You're already registered, please log-in")
+  // if (getUserWithEmail(req.body.email)) {
+  //   res.send("You're already registered, please log-in")
     //would need to implement a delay and redirect but set timeout isn't working
-  } else {
+  // } else {
     let values = [req.body.name, req.body.phone, req.body.email, req.body.password]
     let sqlQuery = `INSERT INTO users(name, phone_number, email, password) VALUES ($1, $2, $3, $4) RETURNING *;`
     res.redirect('/')
     return pool.query(sqlQuery, values).then((res) => res.rows[0]);
-  }
+  // }
 
 });
 
