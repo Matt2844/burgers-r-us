85fe4a1c93a7a2481b6a9a78f3ab788fd54263a8
Ramon Ariel Leon Flores <69723965+arielleon24@users.noreply.github.com>
2020-11-30 11:23:06 -0500

    created object for products (#21)

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,23 +1,41 @@
-INSERT INTO products (name, picture, description, price, category) VALUES ('Nachos', 'undefinedUrl', 'Homemade cheese sauce, salsa, jalapeños.', 11.95, 'Appetizers');
-INSERT INTO products (name, picture, description, price, category) VALUES ('Chili Nachos', 'undefinedUrl', 'Homemade beef chili, jalapeños, homemade cheese sauce, salsa.', 13.95, 'Appetizers');
-INSERT INTO products (name, picture, description, price, category) VALUES ('Cheese Sticks', 'undefinedUrl', 'Mozzarella cheese sticks. Served with marinara sauce', 9.95, 'Appetizers');
-INSERT INTO products (name, picture, description, price, category) VALUES ('Fried Pickles', 'undefinedUrl', 'Served with chipotle Sriracha dip', 7.95, 'Appetizers');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Nachos', './docs/nachos.jpg', 'Homemade cheese sauce, salsa, jalapeños.', 11.95, 'Appetizers');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Chili Nachos', './docs/nachos_chili.jpg', 'Homemade beef chili, jalapeños, homemade cheese sauce, salsa.', 13.95, 'Appetizers');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Cheese Sticks', './docs/cheese_stick.jpg', 'Mozzarella cheese sticks. Served with marinara sauce', 9.95, 'Appetizers');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Fried Pickles', './docs/fried_pickles.jpg', 'Served with chipotle Sriracha dip', 7.95, 'Appetizers');
 
-INSERT INTO products (name, picture, description, price, category) VALUES ('Cheese Burger', 'undefinedURL', 'Classic Cheese Burger served on homemade buns', 13.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Poutine Burger', 'undefinedURL', 'Undecided between a poutine and a burger? Why not both stacked together?', 17.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Burger Rocket', 'undefinedURL', 'Fried Onion rings, bacon, monterey jack, pickles and lettuce. This will take you to the moon!', 19.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Sammy Ds', 'undefinedURL', 'Homemade beef chili, cheddar, tortilla chips. Not sure who Sammy was but he sure made a damn great burger!', 18.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Barnyard', 'undefinedURL', 'Smoky BBQ sauce, bacon, blue cheese, caramelized onions. This is what love taste like', 20.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Cowboy', 'undefinedURL', 'Bacon, mushrooms, caramelized onions, smoky BBQ sauce, cheddar and Monterey Jack. Also served without mushrooms for people with working taste buds', 20.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Burger Tower', 'undefinedURL', '6 quarter-pound beef patties, bacon, 3 slices of Monterey Jack & cheddar because screw that diet!', 32.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Porky', 'undefinedURL', 'Pulled pork, bacon, ham, Monterey Jack, smoky BBQ sauce. Try not to think of that adorable looney tunes Character while you eat this you psycho!', 20.95, 'Burgers' );
-INSERT INTO products (name, picture, description, price, category) VALUES ('Southern comfort', 'undefinedURL', 'Crispy fried chicken, candied bacon, Monterey Jack cheese, maple mayonnaise. Served between two waffles. Your judgment means nothing to us.', 19.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Cheese Burger', './docs/burger2.jpg', 'Classic Cheese Burger served on homemade buns', 13.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Poutine Burger', './docs/Burger_poutine.jpg', 'Undecided between a poutine and a burger? Why not both stacked together?', 17.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Burger Rocket', './docs/Burger_Rocket.jpg', 'Fried Onion rings, bacon, monterey jack, pickles and lettuce. This will take you to the moon!', 19.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Sammy Ds', './docs/Burger_SammyD.jpg', 'Homemade beef chili, cheddar, tortilla chips. Not sure who Sammy was but he sure made a damn great burger!', 18.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Barnyard', './docs/Burger_Banyard', 'Smoky BBQ sauce, bacon, blue cheese, caramelized onions. This is what love taste like', 20.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Cowboy', './docs/Burger_Cowboy.jpg', 'Bacon, mushrooms, caramelized onions, smoky BBQ sauce, cheddar and Monterey Jack. Also served without mushrooms for people with working taste buds', 20.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Burger Tower', './docs/Burger_Tower.jpg', '6 quarter-pound beef patties, bacon, 3 slices of Monterey Jack & cheddar because screw that diet!', 32.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Porky', './docs/Burger_PorkyPig.jpg', 'Pulled pork, bacon, ham, Monterey Jack, smoky BBQ sauce. Try not to think of that adorable looney tunes Character while you eat this you psycho!', 20.95, 'Burgers' );
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Southern comfort', './docs/Burger_Southern.jpg', 'Crispy fried chicken, candied bacon, Monterey Jack cheese, maple mayonnaise. Served between two waffles. Your judgment means nothing to us.', 19.95, 'Burgers' );
 
-INSERT INTO products (name, picture, description, price, category) VALUES ('Mac and Cheese', 'undefinedUrl', 'The classic you know and love. Ask for extra cheese, we wont judge', 9.95, 'Classics');
-INSERT INTO products (name, picture, description, price, category) VALUES ('Poutine', 'undefinedUrl', 'fries, cheese curds and gravy. Can only be ordered in French', 9.95, 'Classics');
-INSERT INTO products (name, picture, description, price, category) VALUES ('Smoked meat poutine', 'undefinedUrl', 'Pulled smoked meat on a classic poutine. Nuff said', 13.95, 'Classics');
-INSERT INTO products (name, picture, description, price, category) VALUES ('Cheesy Dog', 'undefinedUrl', 'Mac N Cheese topped with baked mozzarella. You know you want two of these', 11.95, 'Classics');
-INSERT INTO products (name, picture, description, price, category) VALUES ('Chicken Sub', 'undefinedUrl', 'Grilled chicken, mushrooms, green peppers, caramelized onions topped with aged Mozzarella (Legal age).', 15.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Mac and Cheese', './docs/Mac_Hotdog-mac.jpg', 'The classic you know and love. Ask for extra cheese, we wont judge', 9.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Poutine', './docs/Poutine_Regular.jpg', 'fries, cheese curds and gravy. Can only be ordered in French', 9.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Smoked meat poutine', './docs/Poutine_SmokedMeat.jpg', 'Pulled smoked meat on a classic poutine. Nuff said', 13.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Cheesy Dog', './docs/Mac_Hotdog-mac.jpg', 'Mac N Cheese topped with baked mozzarella. You know you want two of these', 11.95, 'Classics');
+INSERT INTO products (name, picture, description, price, category)
+VALUES ('Chicken Sub', './docs/Sandwich-Chicken.jpg', 'Grilled chicken, mushrooms, green peppers, caramelized onions topped with aged Mozzarella (Legal age).', 15.95, 'Classics');
 
 INSERT INTO products (name, picture, description, price, category)
 VALUES ('Heineken', 'undefinedURL', 'Refreshing pale beer. Served in a coffee cup to anyone ordering before noon',  6.95, 'Beverages');
