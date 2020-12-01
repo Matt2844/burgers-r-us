2c1da4d788ea61c5b3264e8119a13a8f193c0472
Ramon Ariel Leon Flores <69723965+arielleon24@users.noreply.github.com>
2020-12-01 17:25:17 -0500

    Password management (#40)
    * small fixes to unecessary code
    
    * Revert "small fixes to unecessary code"
    
    This reverts commit b7c3cd75ad9e1da331876ac3cb5637feb61873d6.
    
    * changed how email is received and checked in database to avoid Case-sensitivity
    
    * bcrypt testing
    
    * password hashing fully functional
    
    * hashed password for seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,6 @@
--- Users table seeds here (Example)
+-- Users table seeds here (password for these two is the word password)
 INSERT INTO users (name, phone_number, email, password)
-VALUES ('Alice', '514-222-1111', 'AliceWonders@mail.com', 'password');
+VALUES ('Kira', '450-433-5432', 'kiramz@mail.com', '$2b$10$heQ4DbctmlpZJC6IHvMLDuB8Z/sHdcGGv4h3eY5TKYGOEIRmD41Gi');
+
 INSERT INTO users (name, phone_number, email, password)
-VALUES ('Kira', '450-433-5432', 'Kiramz@mail.com', 'password');
+VALUES ('Alice', '514-464-1332', 'alicewonders@mail.com', '$2b$10$heQ4DbctmlpZJC6IHvMLDuB8Z/sHdcGGv4h3eY5TKYGOEIRmD41Gi');
