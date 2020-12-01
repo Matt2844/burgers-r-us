dfef6fb1cfa6d01fe3b74ed93fc8c6da8e7a7cd2
Ramon Ariel Leon Flores <69723965+arielleon24@users.noreply.github.com>
2020-12-01 10:52:30 -0500

    Fixes v2 (#33)
    * small fixes to unecessary code
    
    * Revert "small fixes to unecessary code"
    
    This reverts commit b7c3cd75ad9e1da331876ac3cb5637feb61873d6.
    
    * passed Obj from Database to Server to Index

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const pool = new Pool({
 });
 
 /////////////IMPORT FOR FUNCTIONS IN DATABASE.js
-const {getUserWithEmail } = require('./database')
+const {getUserWithEmail, getUserWithId, productsObj } = require('./database')
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -46,6 +46,7 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const nodemon = require('nodemon');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -58,7 +59,8 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  const templateVars = {user: null, ArrObj: productsObj}
+  res.render("index", templateVars);
 });
 
 //ROUTES BELOW PROBABLY NEED TO BE MOVED
