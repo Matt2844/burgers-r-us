546c3d78d345479084c82452d78c28582e170db3
Matt <70766735+Matt2844@users.noreply.github.com>
2020-12-03 16:31:22 -0500

    garbage bin complete, edited alert message for phone input, deleted useless file (#58)

--- public/scripts/register-client.js
+++ public/scripts/register-client.js
@@ -1,14 +1,14 @@
 // Register Page
 
 
-// Vanilla JS, formats the phone number on register page
+// Formats the phone number on register page
 const formatTel = () => {
   let phoneNum = document.getElementById("phone");
 
   if (phoneNum.value.length < 10) {
-    alert("not enough chars")
+    alert("Please enter a phone number with more digits.")
   } else if (phoneNum.value.length > 14) {
-    alert("too many chars");
+    alert("Please enter a phone number with less digits, or characters.");
   } else if (phoneNum.value.length > 9 && phoneNum.value.length < 15) {
     phoneNum.value = phoneNum.value.replace(/[^0-9]/g, '');
     phoneNum.value = phoneNum.value.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
